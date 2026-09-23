11f452d71b313af171fca5c50f14a4a19c86b00e
akashyap7973 <anukashyap7696@gmail.com>
2026-09-23 13:11:51 +0530

    Remove the international compaliant pdf

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
                 </a>
               </li>
               <li>
-                <a href="https://bmnmsbiymz.ufs.sh/f/1V3V2P4kpAumin6L7sfb2az8t64xBvVQ3dyjGgHlh7Y0fDTo" className="text-gray-300 hover:text-white transition-colors text-sm">
+                <a href="gir " className="text-gray-300 hover:text-white transition-colors text-sm">
                   Internal Complaint Committee
                 </a>
               </li>
